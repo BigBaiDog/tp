766833ab890221cf7f92b64663a26f7034af3478
bigbaidog <1194423126@qq.com>
2022-08-21 15:18:18 +0800

    php

--- public/index.php
+++ public/index.php
@@ -1,24 +1,15 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2019 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
 
-// [ 应用入口文件 ]
-namespace think;
+date_default_timezone_set('Asia/Shanghai');
+
+if (php_sapi_name() === 'cli-server') {
+    $url = parse_url($_SERVER["REQUEST_URI"]);
+    if ($url["path"] != "/" && file_exists(__DIR__ . $url["path"])) {
+        // Static file, serve it as-is
+        return false;
+    }
+}
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . "/../src/app.php";
 
-// 执行HTTP应用并响应
-$http = (new App())->http;
-
-$response = $http->run();
-
-$response->send();
-
-$http->end($response);
